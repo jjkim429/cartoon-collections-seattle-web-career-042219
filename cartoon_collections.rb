12cf144169ff6083b4d7e0f5c4fc92b8c6c65d7b
Jennifer Kim <jjkim429@gmail.com>
2019-04-09 23:29:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ def long_planeteer_calls# code an argument here
   # Your code here
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(snacks)
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
