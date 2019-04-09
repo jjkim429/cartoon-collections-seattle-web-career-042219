3f9caa92f3ac0ff50f4b6c5ded180dc3c11b693f
Jennifer Kim <jjkim429@gmail.com>
2019-04-09 23:33:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ end
 
 def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if snacks.include?(cheese_types) == true
+  if snacks.include?("cheddar", "gouda", "camembert") == true
     return snacks.first
   end
 end
