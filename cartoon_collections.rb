3fefd11b563b4f787f6d7669bb5bbf488500ccc9
Jennifer Kim <jjkim429@gmail.com>
2019-04-09 23:51:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(calls)
 end
 
 def long_planeteer_calls(array)
-  array.any? { |word| word.length 
+  array.any? { |word| word.length > 4
 end
 
 def find_the_cheese(snacks)
