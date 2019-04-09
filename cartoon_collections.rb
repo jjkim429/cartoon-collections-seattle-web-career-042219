90b239c1d6a429045e9c86e7a5ac806d749bf71c
Jennifer Kim <jjkim429@gmail.com>
2019-04-09 23:47:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def summon_captain_planet(calls)
   calls.map! { |x| x.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
   # Your code here
 end
 
